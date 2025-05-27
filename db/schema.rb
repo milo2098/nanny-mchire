559ee0602a824bb519ee4e2c7ca636dc190a2976
Milo Vingoe <milovingoe@hotmail.com>
2025-05-27 15:22:49 +0100

    Grandparent model created and migration ran

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_130135) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_141916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "grandparents", force: :cascade do |t|
+    t.string "skill"
+    t.string "description"
+    t.integer "price"
+    t.integer "experience"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_grandparents_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_130135) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "grandparents", "users"
 end

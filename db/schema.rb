040cb04b3ebc51d5f94f4139e708614b272f2825
Milo Vingoe <milovingoe@hotmail.com>
2025-05-27 15:29:14 +0100

    Booking model created and migration ran

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_27_141916) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_27_142629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "date_time"
+    t.bigint "grandparent_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["grandparent_id"], name: "index_bookings_on_grandparent_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "grandparents", force: :cascade do |t|
     t.string "skill"
@@ -40,5 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_27_141916) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "grandparents"
+  add_foreign_key "bookings", "users"
   add_foreign_key "grandparents", "users"
 end

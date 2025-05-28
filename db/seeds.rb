a8f6b1e4630af796341657fbde218c2e42cd906d
Milo Vingoe <milovingoe@hotmail.com>
2025-05-28 12:20:09 +0100

    Updated seed file with bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,12 @@ user2 = User.create!(first_name: "John", last_name: "Doe", username: "john_doe",
 user3 = User.create!(first_name: "Jane", last_name: "Doe", username: "jane_doe", email: "jade@doe.com", password: "password123")
 user4 = User.create!(first_name: "Milo", last_name: "Vingoe", username: "milo_vingoe", email: "milo@vingoe.com", password: "password123")
 
-Grandparent.create!(skill: "Grardening", description: "I can help you with your garden", price: 20, experience: 5, user: user1)
-Grandparent.create!(skill: "Cooking", description: "I can cook you a delicious meal", price: 30, experience: 10, user: user2)
-Grandparent.create!(skill: "Baking", description: "I can bake you a delicious cake", price: 25, experience: 8, user: user3)
-Grandparent.create!(skill: "Knitting", description: "I can teach you how to knit", price: 15, experience: 3, user: user4)
+grandparent1 = Grandparent.create!(skill: "Grardening", description: "I can help you with your garden", price: 20, experience: 5, user: user1)
+grandparent2 = Grandparent.create!(skill: "Cooking", description: "I can cook you a delicious meal", price: 30, experience: 10, user: user2)
+grandparent3 = Grandparent.create!(skill: "Baking", description: "I can bake you a delicious cake", price: 25, experience: 8, user: user3)
+grandparent4 = Grandparent.create!(skill: "Knitting", description: "I can teach you how to knit", price: 15, experience: 3, user: user4)
+
+Booking.create!(grandparent_id: grandparent1.id, user_id: user4.id, date_time: "2025-10-20 10:00:00")
+Booking.create!(grandparent_id: grandparent2.id, user_id: user3.id, date_time: "2025-10-20 10:00:00")
+Booking.create!(grandparent_id: grandparent3.id, user_id: user2.id, date_time: "2025-10-20 10:00:00")
+Booking.create!(grandparent_id: grandparent4.id, user_id: user1.id, date_time: "2025-10-20 10:00:00")

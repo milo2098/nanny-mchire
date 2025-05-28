a18b720b7777ef969d7db8433798dae82d594aea
Milo Vingoe <milovingoe@hotmail.com>
2025-05-28 15:58:57 +0100

    Can now filter by grandparents skill

--- app/controllers/grandparents_controller.rb
+++ app/controllers/grandparents_controller.rb
@@ -1,6 +1,10 @@
 class GrandparentsController < ApplicationController
   def index
-    @grandparents = Grandparent.all
+    if params.dig(:filter, :skill).present?# || params.dig(:filter, :price).present?
+      @grandparents = Grandparent.where(skill: params[:filter][:skill])#, "price < #{params[:filter][:price]}")
+    else
+      @grandparents = Grandparent.all
+    end
   end
   def show
     @grandparent = Grandparent.find(params[:id])

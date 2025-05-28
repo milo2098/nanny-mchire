03304c895fdd5a19ef5453fd79841833d1e66e8d
Milo Vingoe <milovingoe@hotmail.com>
2025-05-28 13:25:17 +0100

    Bookings new controller action and view created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,9 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
   end
+
+  def new
+    @grandparent = Grandparent.find(params[:grandparent_id])
+    @booking = Booking.new
+  end
 end

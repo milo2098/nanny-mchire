91811e86dad0d7fbf1e010931eb77050accd8d57
Milo Vingoe <milovingoe@hotmail.com>
2025-05-28 12:34:54 +0100

    Bookings route for new and create now nested in grandparents

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :grandparents do
+    resources :bookings, only: [:new, :create]
+  end
   root to: "pages#home"
-  resources :grandparents
-  resources :bookings, only: [:index, :new, :create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

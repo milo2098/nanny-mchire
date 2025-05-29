d5843787a4d0786e92f6b8981fe31ea392fcc051
Milo Vingoe <milovingoe@hotmail.com>
2025-05-29 17:42:47 +0100

    Flatpickr refinement

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,6 +3,18 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element, {
+      mode: "range",
+      enableTime: true,          // <-- Enable time picker
+      time_24hr: true,
+      altInput: true,
+      altFormat: "F j, Y - H:i",
+      dateFormat: "Y-m-d",
+      minDate: "today",
+      minTime: "09:00",
+      maxTime: "19:30",
+      maxDays: 1,
+      minuteIncrement: 30,      // <-- Set minute increment to 30 minutes
+    })
   }
 }
